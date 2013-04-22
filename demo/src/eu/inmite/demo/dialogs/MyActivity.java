8b511aac9b61b80a75867d8ecfeeefdd9d3e17f2
David Vávra <david@inmite.eu>
2013-04-22 15:44:04 +0200

    initial structure

---
+++ demo/src/eu/inmite/demo/dialogs/MyActivity.java
@@ -0,0 +1,15 @@
+package eu.inmite.demo.dialogs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
