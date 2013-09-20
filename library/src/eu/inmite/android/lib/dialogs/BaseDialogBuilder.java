8b5e4f2ccf9d9de7eecad7254b867d0527c030c0
Petr Simek <petr.simek@gmail.com>
2013-09-20 13:53:01 +0200

    tabs

--- library/src/eu/inmite/android/lib/dialogs/BaseDialogBuilder.java
+++ library/src/eu/inmite/android/lib/dialogs/BaseDialogBuilder.java
@@ -46,9 +46,9 @@ abstract class BaseDialogBuilder<T extends BaseDialogBuilder<T>> {
 	
 	public T setCancelableOnTouchOutside(boolean cancelable) {
 		mCancelableOnTouchOutside = cancelable;
-        if (cancelable) {
-            mCancelable = cancelable;
-        }
+		if (cancelable) {
+			mCancelable = cancelable;
+		}
 		return self();
 	}
 
