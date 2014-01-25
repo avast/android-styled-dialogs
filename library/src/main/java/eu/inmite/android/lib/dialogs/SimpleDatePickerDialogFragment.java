0fcfeceec8161b5188fda942f86507403ec4930d
kd7uiy <tuvas21@gmail.com>
2014-01-25 14:11:30 -0500

    Improved the way it manages Time Zones, and allows for passing in a Date object to initialize the values (Preserves non-date fields).

--- library/src/main/java/eu/inmite/android/lib/dialogs/SimpleDatePickerDialogFragment.java
+++ library/src/main/java/eu/inmite/android/lib/dialogs/SimpleDatePickerDialogFragment.java
@@ -2,10 +2,10 @@ package eu.inmite.android.lib.dialogs;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.widget.DatePicker;
@@ -13,6 +13,8 @@ import android.widget.DatePicker;
 public class SimpleDatePickerDialogFragment extends SimpleDialogFragment {
 	
 	DatePicker mDatePicker;
+	Calendar mCalendar;
+	
 	
 	public static SimpleDialogBuilder createBuilder(Context context, FragmentManager fragmentManager) {
 		return new SimpleDialogBuilder(context, fragmentManager, SimpleDatePickerDialogFragment.class);
@@ -24,20 +26,70 @@ public class SimpleDatePickerDialogFragment extends SimpleDialogFragment {
 		mDatePicker=(DatePicker) LayoutInflater.from(getActivity()).inflate(R.layout.dialog_part_datepicker,null);
 		builder.setView(mDatePicker);
 		
+		TimeZone zone=TimeZone.getTimeZone(getArguments().getString("zone"));
+		mCalendar=Calendar.getInstance(zone);
+		mCalendar.setTimeInMillis(getArguments().getLong("date",System.currentTimeMillis()));
+		mDatePicker.updateDate(mCalendar.get(Calendar.YEAR)
+				,mCalendar.get(Calendar.MONTH)
+				,mCalendar.get(Calendar.DAY_OF_MONTH));
 		return builder;
 	}
 	
 	public Date getDate() {
-		return getDate(TimeZone.getTimeZone("GMT") );
+		mCalendar.set(Calendar.YEAR,mDatePicker.getYear());
+		mCalendar.set(Calendar.MONTH,mDatePicker.getMonth());
+		mCalendar.set(Calendar.DAY_OF_MONTH,mDatePicker.getDayOfMonth());
+		return mCalendar.getTime();
 	}
 	
-	public Date getDate(TimeZone zone) {
-		int year=mDatePicker.getYear();
-		int month=mDatePicker.getMonth();
-		int day=mDatePicker.getDayOfMonth();
-		Calendar cal=new GregorianCalendar(zone);
-		cal.set(year,month,day,0,0,0);
-		return cal.getTime();
+	public static class SimpleDialogBuilder extends SimpleDialogFragment.SimpleDialogBuilder {
+
+		Date mDate=new Date();
+		String mTimeZone=null;
+		
+		protected SimpleDialogBuilder(Context context, FragmentManager fragmentManager, Class<? extends SimpleDialogFragment> clazz) {
+			super(context, fragmentManager, clazz);
+		}
+
+		//Remove message from consideration
+		@Override
+		public SimpleDialogBuilder setMessage(int messageResourceId) {
+			return this;
+		}
+
+		//Remove Message from consideration
+		@Override
+		public SimpleDialogBuilder setMessage(int resourceId, Object... formatArgs){
+			return this;
+		}
+
+		//Remove Message from consideration
+		@Override
+		public SimpleDialogBuilder setMessage(CharSequence message) {
+			return this;
+		}
+		
+		public SimpleDialogBuilder setDate(Date date) {
+			mDate=date;
+			return this;
+		}
+		
+		public SimpleDialogBuilder setTimeZone(String zone) {
+			mTimeZone=zone;
+			return this;
+		}
+
+		
+		@Override
+		protected Bundle prepareArguments() {
+			Bundle args=super.prepareArguments();
+			args.putLong("date",mDate.getTime());
+			if (mTimeZone!=null) {
+				args.putString("zone",mTimeZone);
+			} else {
+				args.putString("zone","GMT");
+			}
+			return args;
+		}
 	}
-	
 }
