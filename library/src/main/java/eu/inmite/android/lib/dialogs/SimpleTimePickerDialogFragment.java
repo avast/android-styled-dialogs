7aae8010fe5efb79a5327934fd34b9ca9d0e2433
kd7uiy <tuvas21@gmail.com>
2014-03-06 20:14:32 -0500

    Better format for Date and Time listeners

--- library/src/main/java/eu/inmite/android/lib/dialogs/SimpleTimePickerDialogFragment.java
+++ library/src/main/java/eu/inmite/android/lib/dialogs/SimpleTimePickerDialogFragment.java
@@ -10,7 +10,7 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.widget.TimePicker;
 
-public class SimpleTimePickerDialogFragment extends SimpleDialogFragment {
+public class SimpleTimePickerDialogFragment extends SimpleDatePickerDialogFragment {
 	
 	TimePicker mTimePicker;
 	Calendar mCalendar;
@@ -24,12 +24,12 @@ public class SimpleTimePickerDialogFragment extends SimpleDialogFragment {
 	protected BaseDialogFragment.Builder build(BaseDialogFragment.Builder builder) {
 		builder=super.build(builder);
 		mTimePicker=(TimePicker) LayoutInflater.from(getActivity()).inflate(R.layout.dialog_part_timepicker,null);
-		mTimePicker.setIs24HourView(getArguments().getBoolean("24h"));
+		mTimePicker.setIs24HourView(getArguments().getBoolean(ARG_24H));
 		builder.setView(mTimePicker);
 		
-		TimeZone zone=TimeZone.getTimeZone(getArguments().getString("zone"));
+		TimeZone zone=TimeZone.getTimeZone(getArguments().getString(ARG_ZONE));
 		mCalendar=Calendar.getInstance(zone);
-		mCalendar.setTimeInMillis(getArguments().getLong("date",System.currentTimeMillis()));
+		mCalendar.setTimeInMillis(getArguments().getLong(ARG_DATE,System.currentTimeMillis()));
 
 		mTimePicker.setCurrentHour(mCalendar.get(Calendar.HOUR_OF_DAY));
 		mTimePicker.setCurrentMinute(mCalendar.get(Calendar.MINUTE));
@@ -41,62 +41,5 @@ public class SimpleTimePickerDialogFragment extends SimpleDialogFragment {
 		mCalendar.set(Calendar.HOUR_OF_DAY,mTimePicker.getCurrentHour());
 		mCalendar.set(Calendar.MINUTE,mTimePicker.getCurrentMinute());
 		return mCalendar.getTime();
-	}
-	
-	public static class SimpleDialogBuilder extends SimpleDialogFragment.SimpleDialogBuilder {
-
-		Date mDate=new Date();
-		String mTimeZone=null;
-		boolean m24h=false;
-		
-		protected SimpleDialogBuilder(Context context, FragmentManager fragmentManager, Class<? extends SimpleDialogFragment> clazz) {
-			super(context, fragmentManager, clazz);
-		}
-
-		//Remove message from consideration
-		@Override
-		public SimpleDialogBuilder setMessage(int messageResourceId) {
-			return this;
-		}
-
-		//Remove Message from consideration
-		@Override
-		public SimpleDialogBuilder setMessage(int resourceId, Object... formatArgs){
-			return this;
-		}
-
-		//Remove Message from consideration
-		@Override
-		public SimpleDialogBuilder setMessage(CharSequence message) {
-			return this;
-		}
-		
-		public SimpleDialogBuilder setDate(Date date) {
-			mDate=date;
-			return this;
-		}
-		
-		public SimpleDialogBuilder set24hour(boolean state) {
-			m24h=state;
-			return this;
-		}
-		
-		public SimpleDialogBuilder setTimeZone(String zone) {
-			mTimeZone=zone;
-			return this;
-		}
-		
-		@Override
-		protected Bundle prepareArguments() {
-			Bundle args=super.prepareArguments();
-			args.putLong("date",mDate.getTime());
-			if (mTimeZone!=null) {
-				args.putString("zone",mTimeZone);
-			} else {
-				args.putString("zone","GMT");
-			}
-			args.putBoolean("24h",m24h);
-			return args;
-		}
 	}
 }
